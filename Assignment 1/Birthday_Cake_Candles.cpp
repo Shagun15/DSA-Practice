07e55c2bc087dc15f2397670b590ff398961e99f
Shagun Sharma <60317723+Shagun15@users.noreply.github.com>
2022-07-17 07:18:44 +0530

    Update Birthday_Cake_Candles.cpp

--- Assignment 1/Birthday_Cake_Candles.cpp
+++ Assignment 1/Birthday_Cake_Candles.cpp
@@ -1,18 +1,17 @@
-#include <bits/stdc++.h>
 
-using namespace std;
-int main() {
-    int n,count = 0,tallest = 0; 
-    cin>>n;
-     int a[n];
-    for(int i=0;i<n;i++){
-     cin>>a[i];
-     tallest = max(tallest, a[i]);
-    }
-    for(int i=0;i<n;i++){
-     if(a[i] == tallest) 
-    count++;}
-    cout<<count<<endl;
+int birthdayCakeCandles(vector<int> candles) {
+    int greatest=0,count=0;
     
-    return 0;
+    for(int i=0;i<candles.size();i++){
+        greatest=max(greatest,candles[i]);
+        
+      
+    }
+    for(int i=0;i<candles.size();i++){
+          if(candles[i]==greatest){
+            count++;
+        }
+    }
+return count;
+return 0;
 }
