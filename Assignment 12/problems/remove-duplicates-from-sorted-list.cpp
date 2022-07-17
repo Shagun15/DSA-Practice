42a2fdc869151d0aadebd27c9d51926192ed4812
Shagun Sharma <60317723+Shagun15@users.noreply.github.com>
2022-07-17 17:16:50 +0530

    Create remove-duplicates-from-sorted-list.cpp

---
+++ Assignment 12/problems/remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+      
+        if(head==NULL || head->next==NULL) {
+          return head;
+        }
+      
+        ListNode* prev = head;
+        ListNode* curr = head->next;
+        
+        
+        while(curr && prev){
+          
+            if(prev->val == curr->val)
+            {
+                prev->next = curr->next;
+                curr = prev->next;
+            }
+          
+            else{
+              
+                prev = prev->next;
+                curr = curr->next;
+            }
+        }
+      
+        return head;
+        
+    }
+};
