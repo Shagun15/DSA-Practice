11bd09a10ac34389a76665fc7b68b8b6ab606475
Shagun Sharma <60317723+Shagun15@users.noreply.github.com>
2022-07-19 11:38:29 +0530

    Update Strings.cpp

--- Assignment 2/Strings.cpp
+++ Assignment 2/Strings.cpp
@@ -1,25 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+	string a,b;
+    cin>>a>>b;
+    int len=a.size();
+    int num=b.size();
     
-    string a,b;
-
-    cin>>a;
-    cin>>b;
-
-    int len1 = a.size();
-    int len2 = b.size();
-
-    cout<<len1<<" "<<len2<<endl;
-
-    cout<<a<<b<<endl;
-
-    char c;
-    c=a[0];
+    cout<<len<<" "<<num<<endl;
+    cout<<a+b<<endl;
+    
+    char x;
+    x=a[0];
     a[0]=b[0];
-    b[0]=c;
-
+    b[0]= x;
+    
     cout<<a<<" "<<b;
-return 0;
+  
+    return 0;
 }
