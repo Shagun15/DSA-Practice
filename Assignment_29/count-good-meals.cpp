8a6fe28aecc5c74c2c9c3d14e87fff6f14a4602b
Shagun Sharma <60317723+Shagun15@users.noreply.github.com>
2022-09-07 11:30:40 +0530

    Create count-good-meals.cpp

---
+++ Assignment_29/count-good-meals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+      int countPairs(vector<int>& deliciousness) {
+          
+        unordered_map<int,long long>mp;
+        int n=deliciousness.size();
+        int mod=1e9+7;
+        long long ans=0;
+          
+        for(int s:deliciousness)
+        {
+            for(int i=0; i<=21; i++)
+            {
+               int t=1<<i;
+              
+                if(mp.find(t-s)!=mp.end())
+                {
+                    ans+=mp[t-s];
+                }
+            }
+            mp[s]++;
+        }
+        return ans%mod;
+    }
+};
