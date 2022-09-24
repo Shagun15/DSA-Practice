c1db41f614486a362bca0ece17d9b232aa99820c
Shagun Sharma <60317723+Shagun15@users.noreply.github.com>
2022-09-24 19:36:54 +0530

    Create Counting_Inversion.cpp

---
+++ Assignment_32/Counting_Inversion.cpp
@@ -0,0 +1,51 @@
+long merge(vector<int>&arr, int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int* L = new int[n1];
+    int* R = new int[n2];
+
+    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (int i = 0; i < n2; i++) R[i] = arr[m+1+i];
+
+    int i = 0; int j = 0; int k = l;
+    long count = 0;
+
+    while (i<n1 && j<n2) {
+        if (L[i]<=R[j]) {
+            arr[k] = L[i]; 
+            i++; k++;
+        }
+        else {
+            arr[k] = R[j];
+            j++; k++;
+            count += n1-i;
+    }
+
+    while (i<n1) {
+        arr[k] = L[i];
+        i++; k++;
+    }
+    while (j<n2) {
+        arr[k] = R[j];
+        j++; k++;
+    }
+    return count;
+}
+
+
+long mergeSort(vector<int>&arr, int l, int r) {
+    long count = 0;
+    if (l<r) {
+        int m = (l+r) / 2;
+        count+=mergeSort(arr, l, m);
+        count+=mergeSort(arr, m+1, r);
+        count+=merge(arr, l, m, r);
+    }
+    return count;
+}
+long countInversions(vector<int> &arr) {
+long ans;
+ans =mergeSort(arr,0,arr.size()-1);
+
+return ans;
+}
